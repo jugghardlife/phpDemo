110c1421b9578a0d3a606c1868153eaee879c440
CrazyJugg <601493614@qq.com>
2019-02-11 17:27:58 +0800

    php

---
+++ Home/Conf/Config.php
@@ -0,0 +1,18 @@
+<?php 
+//前端配置数组
+return array(
+	//数据库配置
+	'db_type'=>'mysql',   //数据库类型
+	'db_host'=>'localhost', //主机名
+	'de_port'=>'3306',    //端口号
+	'db_user'=>'root',    //用户名
+	'db_pass'=>'root',    //数据库密码
+	'db_name'=>'itcast',  //数据库名
+	'charset'=>'utf8',    //字符集
+
+
+	//前端默认URL路由参数
+	'default_platform'=>'Home',  //默认应用
+	'default_controller'=>'Index', //默认控制器
+	'default_action'=>'index',   //默认动作
+);
